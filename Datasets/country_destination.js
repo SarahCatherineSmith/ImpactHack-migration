ff3a457f60a9b6c71059117bfbf9939cbf42d535
rabraha3 <rabraha3@gmail.com>
2018-09-22 15:57:15 -0400

    Add dropdown functionality -- change country colors

---
+++ Datasets/country_destination.js
@@ -0,0 +1,112 @@
+var countryOrigin = {
+    'Central African Republic': {
+        'Austria': {
+            'Pct_to_Dest': 0.32063625263314377,
+            'Refugees': 11906
+        },
+        'Germany': {
+            'Pct_to_Dest': 0.7490489442918075,
+            'Refugees': 27814
+        },
+        'Iran': {
+            'Pct_to_Dest': 25.58411221245478,
+            'Refugees': 950000
+        },
+        'Pakistan': {
+            'Pct_to_Dest': 40.52824997670499,
+            'Refugees': 1504912
+        },
+        'Sweden': {
+            'Pct_to_Dest': 0.32559149120902975,
+            'Refugees': 12090
+        }
+    },
+    'Colombia': {
+        'Cameroon': {
+            'Pct_to_Dest': 14.803144113707505,
+            'Refugees': 220702
+        },
+        'Chad': {
+            'Pct_to_Dest': 6.156160688115269,
+            'Refugees': 91783
+        },
+        'Democratic Republic of the Congo': {
+            'Pct_to_Dest': 1.4551486236956817,
+            'Refugees': 21695
+        },
+        'Dem. Rep. of the Congo': {
+            'Pct_to_Dest': 4.572030695285372,
+            'Refugees': 68165
+        },
+        'Sudan': {
+            'Pct_to_Dest': 0.27587122789860974,
+            'Refugees': 4113
+        }
+    },
+    'Eritrea': {
+        'Canada': {
+            'Pct_to_Dest': 0.25631944130225504,
+            'Refugees': 16428
+        },
+        'Costa Rica': {
+            'Pct_to_Dest': 0.2593619474535784,
+            'Refugees': 16623
+        },
+        'Ecuador': {
+            'Pct_to_Dest': 1.8928600962056046,
+            'Refugees': 121317
+        },
+        'Panama': {
+            'Pct_to_Dest': 0.24263596491912393,
+            'Refugees': 15551
+        },
+        'Venezuela': {
+            'Pct_to_Dest': 2.707346794212685,
+            'Refugees': 173519
+        }
+    },
+    'Iraq': {
+        'Ethiopia': {
+            'Pct_to_Dest': 29.68469721532101,
+            'Refugees': 123784
+        },
+        'Israel': {
+            'Pct_to_Dest': 7.834127905303648,
+            'Refugees': 32668
+        },
+        'Sudan': {
+            'Pct_to_Dest': 26.186342315034196,
+            'Refugees': 109196
+        },
+        'Sweden': {
+            'Pct_to_Dest': 3.3830060720006903,
+            'Refugees': 14107
+        },
+        'Switzerland': {
+            'Pct_to_Dest': 3.858790012374219,
+            'Refugees': 16091
+        }
+    },
+    'Somalia': {
+        'Germany': {
+            'Pct_to_Dest': 1.0027532051102488,
+            'Refugees': 41167
+        },
+        'Iran': {
+            'Pct_to_Dest': 0.7794617670349543,
+            'Refugees': 32000
+        },
+        'Jordan': {
+            'Pct_to_Dest': 0.7127934277732458,
+            'Refugees': 29263
+        },
+        'Sweden': {
+            'Pct_to_Dest': 0.5890782304366666,
+            'Refugees': 24184
+        },
+        'Syria': {
+            'Pct_to_Dest': 3.561165948140947,
+            'Refugees': 146200
+        }
+    }
+};
--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@ countriesData.features.map(val => {
     
 });
 console.log("For Zambia ... ", asylum['Zambia'])
-
+var selectedCountry = "";
 var map = L.map('map').setView([0.0, 0.0], 2);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     }).addTo( map );
 
@@ -34,10 +34,11 @@ info.update = function (props) {
 
 info.addTo(map);
 
-
 // get color depending on budget value
-function getColor(d) {
-    return d > 10 ? '#000000' :
+function getColor(feature) {
+    if (selectedCountry == "") {
+        var d = feature.properties.data;
+        return d > 10 ? '#000000' :
            d > 5  ? '#000000' :
            d > 2  ? '#111111' :
            d > 1  ? '#333333' :
@@ -46,37 +47,137 @@ function getColor(d) {
            d > 0.1  ? '#dddddd' :
            d >= 0 ? '#eeeeee' :
                       '#ffffff';
-    // return d > 100000000 ? '#800026' :
-    //        d > 50000000  ? '#BD0026' :
-    //        d > 20000000  ? '#E31A1C' :
-    //        d > 10000000  ? '#FC4E2A' :
-    //        d > 5000000   ? '#FD8D3C' :
-    //        d > 2000000   ? '#FEB24C' :
-    //        d > 1000000  ? '#FED976' :
-    //                   '#FFEDA0';
+    } else if (selectedCountry == "Colombia") {
+        switch (feature.properties.name){
+            case selectedCountry:
+                return "#FF0000";
+            case "Cameroon":
+                return "#000000";
+            case "Chad":
+                return "#000000";
+            case "Democratic Republic of the Congo":
+                return "#000000";
+            case "Sudan":
+                return "#000000";
+            default: 
+                return "#CCCCCC";
+        }
+    } else if (selectedCountry == "Eritrea") {
+        switch (feature.properties.name) {
+            case selectedCountry:
+                return "#FF0000";
+            case 'Canada':
+                return "#000000";
+            case 'Costa Rica':
+                return "#000000";
+            case 'Ecuador':
+                return "#000000";
+            case 'Panama':
+                return "#000000";
+            case 'Venezuela':
+                return "#000000";
+            default:
+                return "#CCCCCC";
+        }
+    } else if (selectedCountry == "Iraq") {
+        switch (feature.properties.name) {
+            case selectedCountry:
+                return "#FF0000";
+            case 'Ethiopia':
+                return "#000000"
+            case 'Israel':
+                return "#000000"    
+            case 'Sudan':
+                return "#000000"    
+            case 'Sweden':
+                return "#000000"    
+            case 'Switzerland':
+                return "#000000";
+            default:
+                return "#CCCCCC";
+        }
+    } else if (selectedCountry == "Somalia") {
+        switch (feature.properties.name) {
+            case selectedCountry:
+                return "#FF0000";
+                case 'Germany':
+                    return "#000000";
+                case 'Iran':
+                    return "#000000";
+                case 'Jordan':
+                    return "#000000";
+                case 'Sweden':
+                    return "#000000";
+                case 'Syria':
+                    return "#000000";
+                default:
+                    return "#CCCCCC";
+        }
+    } else if (selectedCountry == "Central African Republic") {
+        switch (feature.properties.name) {
+            case selectedCountry:
+                return "#FF0000";
+                case 'Austria':
+                    return "#000000";
+                case 'Germany':
+                    return "#000000";
+                case 'Iran':
+                    return "#000000";
+                case 'Pakistan':
+                    return "#000000";
+                case 'Sweden':
+                    return "#000000";
+                default:
+                    return "#CCCCCC";
+        }
+    }
+    
 }
 
 function style(feature) {
-    if(feature.properties.data > 0) {
-    return {
-        weight: 1,
-        opacity: 1,
-        color: 'white',
-        dashArray: '',
-        fillOpacity: 0.7,
-        fillColor: getColor(feature.properties.data)
+    if (selectedCountry == ""){
+            return {
+                weight: 1,
+                opacity: 1,
+                color: 'white',
+                dashArray: '',
+                fillOpacity: 0.7,
+                fillColor: getColor(feature)
+            }
+        // }
+        // else {
+        //     return {
+        //         weight: 0,
+        //         opacity: 0,
+        //         color: 'red',
+        //         dashArray: '',
+        //         fillOpacity: 0.0,
+        //         fillColor: getColor(feature)
+        //     }
+        // };
+    } else {
+        // if(feature.properties.data > 0) {
+            return {
+                weight: 1,
+                opacity: 1,
+                color: 'white',
+                dashArray: '',
+                fillOpacity: 1,
+                fillColor: getColor(feature)
+            }
+        // }
+        // else {
+        //     return {
+        //         weight: 0,
+        //         opacity: 0,
+        //         color: 'red',
+        //         dashArray: '',
+        //         fillOpacity: 0.0,
+        //         fillColor: getColor(feature)
+        //     }
+        // };
     }
-    }
-    else {
-    return {
-        weight: 0,
-        opacity: 0,
-        color: 'red',
-        dashArray: '',
-        fillOpacity: 0.0,
-        fillColor: getColor(feature.properties.data)
-    }
-    };
+    
 }
 
 function highlightFeature(e) {
@@ -108,13 +209,15 @@ function zoomToFeature(e) {
 }
 
 function onEachFeature(feature, layer) {
-    if(feature.properties.budget > 0) {
+    if(feature.properties.data >= 0) {
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
-        //click: zoomToFeature
+        // click: zoomToFeature
     });
-    layer.bindPopup(feature.properties.name);
+    var stringToAdd = "" + feature.properties.name + "<br/>";
+    stringToAdd += feature.properties.data + "<br/>";
+    layer.bindPopup(stringToAdd);
     }
 }
 
@@ -153,6 +256,18 @@ legend.addTo(map);
 
 
 function changeCountry(country) {
-    console.log("Selected element", country);
-    console.log("Selected country name = ", country.value);
+    console.log(geojson);
+    selectedCountry = country.value;
+    console.log("Selected Country = ", selectedCountry);
+    geojson.setStyle(style)
+    // var name = country.value;
+    // var ret = "";
+
+    // var topFive = countryOrigin[name];
+
+    // Object.keys(topFive).forEach((key, index) => {
+    //     ret += (index+1) + ". " + topFive[key]["Pct_to_Dest"] + "<br/>";
+    // })
+
 }
+
